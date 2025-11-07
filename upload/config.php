0266e00b99a7dad88e1101c1c628c7a152862d93
meiqiu <119494143+MEIQIUawa@users.noreply.github.com>
2025-11-08 03:02:06 +0800

    Update database host in Config class

--- upload/config.php
+++ upload/config.php
@@ -23,7 +23,7 @@ class Config {
     public static function getDB() {
         if (self::$db === null) {
             try {
-                self::$db = new PDO("mysql:host=156.233.233.146;port=3306;dbname=test123;charset=utf8mb4", "test123", "test123", [
+                self::$db = new PDO("mysql:host=1.2.3.4;port=3306;dbname=test123;charset=utf8mb4", "test123", "test123", [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES => false
@@ -35,4 +35,5 @@ class Config {
         return self::$db;
     }
 }
+
 ?>
